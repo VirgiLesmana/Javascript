e1182ab3f85336df4c719a98788a250583bcdfba
VirgiLesmana <68315824+VirgiLesmana@users.noreply.github.com>
2021-01-12 22:05:55 +0700

    Add files via upload

---
+++ javascript/js/logika.js
@@ -0,0 +1,31 @@
+let a = true;
+let b = false;
+let c = true;
+let d = false;
+//OR
+//akan bernilai true jika salah satu true
+console.log(d || b || a);
+//AND
+//akan bernilai false jika salah satu false
+console.log(a && b && c);
+
+let x = 8;
+let y = 7;
+//Lebih besar
+console.log(x > y);
+//Lebih kecil
+console.log(x < y);
+//Sama dengan
+console.log(x == y);
+//Lebih besar sama dengan
+console.log(x >= y);
+//Lebih kecil sama dengan
+console.log(x <= y);
+
+let lima = "5";
+let limo = 5;
+//Identik - Sama type data dan isi
+console.log(lima === limo);
+
+
+
